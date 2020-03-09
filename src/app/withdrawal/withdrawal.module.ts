d609ab47c86552205f8fa27bbf019d159b45e4b8
hnakao <hnakao11@gmail.com>
2020-03-09 15:18:20 -0400

    Fix withdrawal dependencies

--- src/app/withdrawal/withdrawal.module.ts
+++ src/app/withdrawal/withdrawal.module.ts
@@ -2,17 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../common/auth/auth.module';
 import { UserModule } from '../user/user.module';
 import { SoapModule } from '../soap/soap.module';
-import { OrderService } from '../order/order.service';
-import { ManifestService } from '../manifest/manifest.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Withdrawal } from './withdrawal.entity';
 import { WithdrawalRepository } from './withdrawal.repository';
 import { WithdrawalController } from './withdrawal.controller';
 import { WithdrawalService } from './withdrawal.service';
-import { AdmissionService } from '../admission/admission.service';
-import { FulfillmentService } from '../fulfillment/fulfillment.service';
 import { OrderModule } from '../order/order.module';
-import { ManifestModule } from '../manifest/manifest.module';
+import { FulfillmentModule } from '../fulfillment/fulfillment.module';
 
 @Module({
     imports: [
@@ -21,15 +17,11 @@ import { ManifestModule } from '../manifest/manifest.module';
         UserModule,
         SoapModule,
         OrderModule,
-        ManifestModule,
+        FulfillmentModule,
     ],
     controllers: [WithdrawalController],
     providers: [
         WithdrawalService,
-        OrderService,
-        ManifestService,
-        AdmissionService,
-        FulfillmentService,
     ],
     exports: [WithdrawalService],
 })
