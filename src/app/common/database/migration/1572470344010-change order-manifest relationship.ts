e2a2408cc7dccca6eda366629a0568fa4417793a
hnakao <hnakao11@gmail.com>
2020-03-09 13:36:50 -0400

    Fix some migrations

--- src/app/common/database/migration/1572470344010-change order-manifest relationship.ts
+++ src/app/common/database/migration/1572470344010-change order-manifest relationship.ts
@@ -3,9 +3,6 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class changeOrderManifestRelationship1572470344010 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`ALTER TABLE "order" DROP CONSTRAINT "FK_1de1fb8bcefd61e67dfda081b1c"`);
-        await queryRunner.query(`ALTER TABLE "order" DROP CONSTRAINT "UQ_1de1fb8bcefd61e67dfda081b1c"`);
-        await queryRunner.query(`ALTER TABLE "order" DROP COLUMN "manifest_id"`);
         await queryRunner.query(`ALTER TABLE "manifest" ADD "order_id" uuid`);
         await queryRunner.query(`ALTER TABLE "manifest" ADD CONSTRAINT "UQ_f42819c4ec27db7ae945e6ae209" UNIQUE ("order_id")`);
         await queryRunner.query(`ALTER TABLE "manifest" ADD CONSTRAINT "FK_f42819c4ec27db7ae945e6ae209" FOREIGN KEY ("order_id") REFERENCES "order"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
@@ -15,9 +12,6 @@ export class changeOrderManifestRelationship1572470344010 implements MigrationIn
         await queryRunner.query(`ALTER TABLE "manifest" DROP CONSTRAINT "FK_f42819c4ec27db7ae945e6ae209"`);
         await queryRunner.query(`ALTER TABLE "manifest" DROP CONSTRAINT "UQ_f42819c4ec27db7ae945e6ae209"`);
         await queryRunner.query(`ALTER TABLE "manifest" DROP COLUMN "order_id"`);
-        await queryRunner.query(`ALTER TABLE "order" ADD "manifest_id" uuid`);
-        await queryRunner.query(`ALTER TABLE "order" ADD CONSTRAINT "UQ_1de1fb8bcefd61e67dfda081b1c" UNIQUE ("manifest_id")`);
-        await queryRunner.query(`ALTER TABLE "order" ADD CONSTRAINT "FK_1de1fb8bcefd61e67dfda081b1c" FOREIGN KEY ("manifest_id") REFERENCES "manifest"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
 }
--- src/app/common/database/migration/1574196484712-modify some orders fields to decimals.ts
+++ src/app/common/database/migration/1574196484712-modify some orders fields to decimals.ts
@@ -5,16 +5,12 @@ export class modifySomeOrdersFieldsToDecimals1574196484712 implements MigrationI
     public async up(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query(`ALTER TABLE "order" DROP COLUMN "totalWeight"`);
         await queryRunner.query(`ALTER TABLE "order" ADD "totalWeight" numeric(5,2)`);
-        await queryRunner.query(`ALTER TABLE "order" DROP COLUMN "kg"`);
         await queryRunner.query(`ALTER TABLE "order" ADD "kg" numeric(3,2)`);
-        await queryRunner.query(`ALTER TABLE "order" DROP COLUMN "volumen"`);
         await queryRunner.query(`ALTER TABLE "order" ADD "volumen" numeric(7,6)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`ALTER TABLE "order" DROP COLUMN "volumen"`);
         await queryRunner.query(`ALTER TABLE "order" ADD "volumen" integer`);
-        await queryRunner.query(`ALTER TABLE "order" DROP COLUMN "kg"`);
         await queryRunner.query(`ALTER TABLE "order" ADD "kg" integer`);
         await queryRunner.query(`ALTER TABLE "order" DROP COLUMN "totalWeight"`);
         await queryRunner.query(`ALTER TABLE "order" ADD "totalWeight" integer`);
